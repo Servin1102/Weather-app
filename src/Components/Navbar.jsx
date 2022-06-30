62cee204ec72edbb715759ad179d7f63da4d02c9
Juan Martinez <juanmaa1102@gmail.com>
2022-06-30 09:50:34 -0500

    changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,15 +8,8 @@ const Navbar = () => {
             <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-center">
                 <a href="/"><img src="./src/images/weather_43796.png" alt="" /></a>
                     <div className='col-12 d-flex justify-content-center'>
-                        <h1>Weather</h1>
+                        <h1>Weather APP</h1>
                     </div>
-                    
-        {/* <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" className="form-control form-control-dark text-white bg-dark" placeholder="City..." aria-label="Search" /> 
-        </form>
-        <div className="text-end">
-          <button type="button" className="btn btn-warning">Search</button>
-        </div> */}
       </div>
     </div>
   </header>
--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -8,10 +8,14 @@ const Weather = () => {
 
     const [ city, setcity ] = useState({})
 
+    
+    
+
     useEffect(() =>{
         const success = pos =>{
             const long = pos.coords.longitude
             const lat = pos.coords.latitude
+                       
             axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=bb16750571e6c29de28f9e3d1a04ce0f`)
             .then(res => setcity(res.data))
         }
@@ -20,15 +24,32 @@ const Weather = () => {
     }, [])
     console.log(city);
 
+    
     return (
         <div className='container p-5'>
-            <div className="card bg-flowers">
-                <div className="d-flex justify-content-center p-4">
-                <h1>{city.name}, {city.sys.country}</h1>
+            <div className="card bg-card">
+                <div className="d-flex justify-content-center p-5">
+                    <h2>{city.name}, {city.sys?.country}</h2>
                 </div>
-               
-
-                
+                <div className='row m-0'>
+                    <div className='col-6 text-center'>
+                        <img src={`http://openweathermap.org/img/wn/${city.weather?.[0].icon}@2x.png`} className="icon" alt="" />
+                    </div>
+                    <div className='col-6 text-center'>
+                        <p>"{city.weather?.[0].description}"</p>
+                        <div className='align-bottom'>
+                            <p><i className="fas fa-wind"></i><b> Wind speed: </b> {city.wind?.speed} m/s</p>
+                            <p><i className="fas fa-cloud"></i><b> Clouds: </b> {city.clouds?.all}%</p>
+                            <p><i className="fas fa-temperature-high"></i><b> Pressure: </b> {city.main?.pressure} mb</p>
+                        </div>
+                    </div>
+                    <div className="col-6 text-center">
+                    <h3> {city.main?.temp}°</h3>
+                    </div>
+                    <div className='col-12 text-center p-5'>
+                        <button  className='btn-weather'>°F / °C</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
