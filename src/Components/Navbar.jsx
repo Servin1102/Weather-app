8aa524da66a81986f1001b86433a6e25ee3b8d2d
Juan Martinez <juanmaa1102@gmail.com>
2022-07-01 10:48:53 -0500

    changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header className="p-3 bg-dark text-white">
         <div className="container">
             <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-center">
-                <a href="/"><img src="./src/images/weather_43796.png" alt="" /></a>
+                <a href="/"><img src="./src/images/weather-43796.png" alt="" /></a>
                     <div className='col-12 d-flex justify-content-center'>
                         <h1><b> Weather APP</b></h1>
                     </div>
